ae13c3375860bfe3e267a49a8f1a5ed6581c510f
Daniel Gonzalez <dgonzaleztx1990@gmail.com>
2023-03-25 13:55:02 -0500

    created buttons to change canvas size

--- scripts.js
+++ scripts.js
@@ -6,11 +6,52 @@ for (x = 0; x < 256; x++) {
     container.appendChild(divs);
 }
 
-const grid = document.querySelectorAll('.grid');
+
+function smallCanvus () {
+    container.innerHTML="";
+    for (x = 0; x < 256; x++) {
+        const divs = document.createElement('div');
+        divs.classList.add('grid');
+        const divsReplace = document.querySelector('.grid')
+        divsReplace.replaceWith(divs);
+    }
+}
+
+const smallButton = document.querySelector('#small');
+smallButton.addEventListener('click',smallCanvus)
+
+function mediumCanvus () {
+    container.innerHTML="";
+    for (x = 0; x < 1024; x++) {
+        const divs = document.createElement('div');
+        divs.classList.add('mediumGrid');
+        const divsReplace = document.querySelector('.grid')
+        divsReplace.replaceWith(divs);
+    }
+}
+
+const mediumButton = document.querySelector('#medium');
+mediumButton.addEventListener('click',mediumCanvus)
+
+function largeCanvus () {
+    container.innerHTML="";
+    for (x = 0; x < 4096; x++) {
+        const divs = document.createElement('div');
+        divs.classList.add('largeGrid');
+        const divsReplace = document.querySelector('.largeGrid')
+        divsReplace.replaceWith(divs);
+    }
+}
+const largeButton = document.querySelector('#large');
+largeButton.addEventListener('click',largeCanvus)
+
+const grid = document.querySelectorAll('#container.div');
 
 grid.forEach(element => {
     element.addEventListener('mouseenter', draw)
 });
 function draw(e){
-    e.target.classList.add('ink');
+    e.target.style.backgroundColor = 'black';
+    console.log(e)
 }
+
